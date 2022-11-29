a6fa382d8995c5d0dd5b5ef93d1f786cd1c62628
Park Sunjoo <w2118@e-mirim.hs.kr>
2022-11-29 21:08:56 +0900

    db생성. html연결

--- search/models.py
+++ search/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Jobs(models.Model):
+    enterprise = models.CharField(max_length=50) #기업명
+    enterprise_num = models.CharField(max_length=50) #사업자등록번호
+    sector = models.CharField(max_length=50) #업종
+    employee = models.CharField(max_length=50) #사원수(명)
+    sales = models.CharField(max_length=50) #매출규모(억)
+    address = models.CharField(max_length=100) #주소(도로명 주소)
+    weblink = models.CharField(max_length=200) #웹사이트 주소
+    introduce = models.CharField(max_length=500) #기업소개 및 기업규몬
+    work = models.CharField(max_length=50) # 담당업무
+    req_apply = models.CharField(max_length=200) #지원요건
+    recruitment = models.CharField(max_length=50) #채용인원
+    submit = models.CharField(max_length=50) #제출서류
+    submit_deadline = models.CharField(max_length=100) #서류제출 마감일
+    etc1 = models.CharField(max_length=200) #기타사항들...
+    etc2 = models.CharField(max_length=200)
+    etc3 = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.sector
--- search/views.py
+++ search/views.py
@@ -1,6 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import Jobs
 
 def index(request):
     return render(request, 'static/base.html')
+
+def searchBar(request):
+    if request.method == 'GET':
+        query = request.GET.get('query')
+        if query: #query, 검색어가 있을 시
+            sector = Jobs.objects.filter(name__contains=query)
+            return render(request, 'searchbar.html', {'sector': sector})
+        else: #없을 시
+            print("No infomation to show")
+            return request(request, 'searchbar.html', {})
